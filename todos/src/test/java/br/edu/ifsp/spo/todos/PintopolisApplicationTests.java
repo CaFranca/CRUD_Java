4b93a01ccea53de5adbdef9b6e703c0fa1c0e674
Caíque <135059792+CaFranca@users.noreply.github.com>
2025-06-18 08:05:47 -0300

    o inicio de um novo tipo de Web

---
+++ todos/src/test/java/br/edu/ifsp/spo/todos/PintopolisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.spo.todos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PintopolisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
