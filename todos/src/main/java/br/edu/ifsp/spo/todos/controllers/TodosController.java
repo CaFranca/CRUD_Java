413edf2ff9d8c0b8ee24a244e63b5192486e3b98
Caíque <135059792+CaFranca@users.noreply.github.com>
2025-06-18 10:13:40 -0300

    Add files via upload

---
+++ todos/src/main/java/br/edu/ifsp/spo/todos/controllers/TodosController.java
@@ -0,0 +1,33 @@
+package br.edu.ifsp.spo.todos.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
+record Todo(String texto, boolean concluido){}
+record TodoForm(String texto){}
+
+@Controller
+public class TodosController {
+    List<Todo> todos = Arrays.asList(
+            new Todo("Tarefa 1", false),
+            new Todo("Tarefa 2", false),
+            new Todo("Tarefa 3", false)
+    );
+    @GetMapping("/todos")
+    public String index(Model model){
+        model.addAttribute("todoList", todos);
+        return "todos/index.html";
+    }
+
+    @PostMapping("/todos")
+    public String create(Model model, TodoForm form){
+        form.texto();
+
+        return "??????";
+    }
+}
